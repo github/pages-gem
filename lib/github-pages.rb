6ac2bfb0c3032a34f0de571ab48de19459548512
Parker Moore <parkrmoore@gmail.com>
2014-11-11 23:14:51 -0800

    Bump jekyll-sitemap to v0.6.3
    This release is about backwards-compatibility for versions of Jekyll before 2.5.0.
    Release: https://github.com/jekyll/jekyll-sitemap/releases/tag/v0.6.3
    Diff: https://github.com/jekyll/jekyll-sitemap/compare/v0.6.2...v0.6.3

--- lib/github-pages.rb
+++ lib/github-pages.rb
@@ -28,7 +28,7 @@ class GitHubPages
       "jemoji"                => "0.3.0",
       "jekyll-mentions"       => "0.1.3",
       "jekyll-redirect-from"  => "0.6.2",
-      "jekyll-sitemap"        => "0.6.2",
+      "jekyll-sitemap"        => "0.6.3",
     }
   end
 
