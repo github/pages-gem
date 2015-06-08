82cce94af148741527b77572034f64f6d2c5de81
Parker Moore <parkrmoore@gmail.com>
2015-06-08 09:23:39 -0700

    Upgrade pygments.rb from 0.6.1 to 0.6.3

--- lib/github-pages.rb
+++ lib/github-pages.rb
@@ -22,7 +22,7 @@ class GitHubPages
       "liquid"                => "2.6.2",
 
       # Highlighters
-      "pygments.rb"           => "0.6.1",
+      "pygments.rb"           => "0.6.3",
 
       # Plugins
       "jemoji"                => "0.4.0",
