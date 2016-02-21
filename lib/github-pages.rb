3024ea7d38aaf3a01437fe2bfcc592f602708c3a
Ben Balter <ben.balter@github.com>
2016-02-21 14:47:07 -0500

    bump jekyll seo tag to 1.2.0

--- lib/github-pages.rb
+++ lib/github-pages.rb
@@ -33,7 +33,7 @@ module GitHubPages
       "jekyll-paginate"           => "1.1.0",
       "github-pages-health-check" => "1.0.1",
       "jekyll-coffeescript"       => "1.0.1",
-      "jekyll-seo-tag"            => "1.0.0",
+      "jekyll-seo-tag"            => "1.2.0",
     }
   end
 
