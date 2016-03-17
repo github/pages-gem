b7609a36fd01802db0979f0f7b3efd4927c046da
Parker Moore <parkrmoore@gmail.com>
2016-03-17 12:09:05 -0700

    Output jekyll & github-pages versions to Jekyll debug logger

--- lib/github-pages.rb
+++ lib/github-pages.rb
@@ -5,12 +5,15 @@ require "jekyll"
 module GitHubPages
   autoload :Configuration, "github-pages/configuration"
   autoload :Dependencies,  "github-pages/dependencies"
+  autoload :VERSION,       "github-pages/version"
 
   def self.versions
     Dependencies.versions
   end
 end
 
+Jekyll.logger.debug "GitHub Pages:", "github-pages v#{GitHubPages::VERSION}"
+Jekyll.logger.debug "GitHub Pages:", "jekyll v#{Jekyll::VERSION}"
 Jekyll::Hooks.register :site, :after_reset do |site|
   GitHubPages::Configuration.set(site)
 end
