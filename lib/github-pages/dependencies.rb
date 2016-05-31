cd16a77b288375f9cc24b0119c315f2b1cffd9c2
Ben Balter <ben.balter@github.com>
2016-05-31 15:20:47 -0400

    remove merge conflict

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -25,13 +25,8 @@ module GitHubPages
       "jekyll-gist"               => "1.4.0",
       "jekyll-paginate"           => "1.1.0",
       "jekyll-coffeescript"       => "1.0.1",
-<<<<<<< HEAD
       "jekyll-seo-tag"            => "2.0.0",
-      "jekyll-github-metadata"    => "1.11.1",
-=======
-      "jekyll-seo-tag"            => "1.4.0",
       "jekyll-github-metadata"    => "2.0.0",
->>>>>>> master
 
       # Pin listen because it's broken on 2.1 & that's what we recommend.
       # https://github.com/guard/listen/pull/371
