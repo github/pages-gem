435ec5bfbc1e4ede412d4ea56c2b12bcdc86cb9d
Parker Moore <parkrmoore@gmail.com>
2016-07-15 15:28:35 -0700

    dependencies: pin activesupport to v4.2.7
    Temporary fix until these are fixed:
    
    - https://github.com/jekyll/jemoji/issues/46
    - https://github.com/jekyll/jekyll-mentions/issues/36

--- spec/github-pages-dependencies_spec.rb
+++ spec/github-pages-dependencies_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe(GitHubPages::Dependencies) do
   CORE_DEPENDENCIES = %w(
     jekyll kramdown liquid rouge jekyll-sass-converter
-    github-pages-health-check listen
+    github-pages-health-check listen activesupport
   ).freeze
   PLUGINS = described_class::VERSIONS.keys - CORE_DEPENDENCIES
 
