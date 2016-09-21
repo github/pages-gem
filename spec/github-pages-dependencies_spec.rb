3d7339b0d77607a9f36c25cb9a67bd42ea20fd7d
Parker Moore <parkrmoore@gmail.com>
2016-09-21 15:45:43 -0700

    Refactor themes into a separate hash.

--- spec/github-pages-dependencies_spec.rb
+++ spec/github-pages-dependencies_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe(GitHubPages::Dependencies) do
   CORE_DEPENDENCIES = %w(
     jekyll kramdown liquid rouge jekyll-sass-converter
-    github-pages-health-check listen activesupport minima
+    github-pages-health-check listen activesupport
   ).freeze
   PLUGINS = described_class::VERSIONS.keys - CORE_DEPENDENCIES
 
@@ -30,6 +30,15 @@ describe(GitHubPages::Dependencies) do
     PLUGINS.each do |plugin|
       it "whitelists the #{plugin} plugin" do
         expect(GitHubPages::Configuration::PLUGIN_WHITELIST).to include(plugin)
+      end
+    end
+  end
+
+  context "themes" do
+    described_class::VERSIONS.each do |(theme, version)|
+      it "depends on #{theme}" do
+        expect(described_class.gems[theme]).to be_a(String)
+        expect(described_class.gems[theme]).to eq(version)
       end
     end
   end
