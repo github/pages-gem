70caec5907e7f965df19d377294ed115db22b44f
Hubot <hubot@users.noreply.github.com>
2016-10-05 12:24:56 -0500

    Bump jekyll-github-metadata :gem: to v2.1.0

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -26,7 +26,7 @@ module GitHubPages
       "jekyll-paginate"           => "1.1.0",
       "jekyll-coffeescript"       => "1.0.1",
       "jekyll-seo-tag"            => "2.0.0",
-      "jekyll-github-metadata"    => "2.0.2",
+      "jekyll-github-metadata"    => "2.1.0",
 
       # Pin listen because it's broken on 2.1 & that's what we recommend.
       # https://github.com/guard/listen/pull/371
