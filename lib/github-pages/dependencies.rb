53ba6d1d335e46fde358f29f0e956fd742852429
Parker Moore <parkrmoore@gmail.com>
2016-10-05 12:08:56 -0700

    jekyll v3.3.0.pre.rc1

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -5,7 +5,7 @@ module GitHubPages
   class Dependencies
     VERSIONS = {
       # Jekyll
-      "jekyll"                    => "3.3.0",
+      "jekyll"                    => "3.3.0.pre.rc1",
       "jekyll-sass-converter"     => "1.3.0",
 
       # Converters
