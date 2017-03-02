8cffd90d8c735177f0ac36211dda6ce20594c81e
Parker Moore <parkrmoore@gmail.com>
2017-03-02 13:29:28 -0500

    Upgrade to jekyll v3.4.1

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -6,7 +6,7 @@ module GitHubPages
   class Dependencies
     VERSIONS = {
       # Jekyll
-      "jekyll"                    => "3.4.0",
+      "jekyll"                    => "3.4.1",
       "jekyll-sass-converter"     => "1.5.0",
 
       # Converters
