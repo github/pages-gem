a53b8762fc1450a89f5df04eb089de6cd1d32845
Parker Moore <parkr@github.com>
2019-11-22 10:15:51 -0500

    Update jekyll-readme-index to v0.3.0

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -36,7 +36,7 @@ module GitHubPages
       "jekyll-mentions" => "1.5.1",
       "jekyll-relative-links" => "0.6.1",
       "jekyll-optional-front-matter" => "0.3.2",
-      "jekyll-readme-index" => "0.2.0",
+      "jekyll-readme-index" => "0.3.0",
       "jekyll-default-layout" => "0.1.4",
       "jekyll-titles-from-headings" => "0.5.1",
 
