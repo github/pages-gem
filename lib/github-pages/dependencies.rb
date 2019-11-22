265a8dfc41b56fd4aecc691d9676247e5bdb4868
Parker Moore <parkr@github.com>
2019-11-22 10:02:59 -0500

    Update jekyll-seo-tag to v2.6.1

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -26,7 +26,7 @@ module GitHubPages
       "jekyll-gist" => "1.5.0",
       "jekyll-paginate" => "1.1.0",
       "jekyll-coffeescript" => "1.1.1",
-      "jekyll-seo-tag" => "2.5.0",
+      "jekyll-seo-tag" => "2.6.1",
       "jekyll-github-metadata" => "2.12.1",
       "jekyll-avatar" => "0.6.0",
       "jekyll-remote-theme" => "0.4.0",
