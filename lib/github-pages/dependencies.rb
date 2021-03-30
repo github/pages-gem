24de1be972100ed084d8a07ac0c78529e3dcea3c
Dave Ungerer <daveungerer@gmail.com>
2021-03-30 11:01:41 +0800

    CVE-2021-28834 kramdown gem security update

--- lib/github-pages/dependencies.rb
+++ lib/github-pages/dependencies.rb
@@ -11,7 +11,7 @@ module GitHubPages
       "jekyll-sass-converter" => "1.5.2",
 
       # Converters
-      "kramdown" => "2.3.0",
+      "kramdown" => "2.3.1",
       "kramdown-parser-gfm" => "1.1.0",
       "jekyll-commonmark-ghpages" => "0.1.6",
 
